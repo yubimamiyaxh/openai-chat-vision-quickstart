74fcd4efb1ba4bb26e08eb3927988601dd395991
yubimamiyaxh <yubi.mamiya@xenonhealth.com>
2025-07-11 15:25:06 +0000

    increase dpi resolution of pdf to image conversion, decrease batch size to 1, make schemas less verbose

--- src/quartapp/chat.py
+++ src/quartapp/chat.py
@@ -137,7 +137,9 @@ async def index():
 
 # Convert a PyMuPDF page to a PIL image.
 async def convert_pdf_page_to_image(page):
-    pix = page.get_pixmap(dpi=100)
+    # YUBI: debugging statement, want to change back alter
+    # updated dpi from 100 to 200 for payment to see if it improves accuracy
+    pix = page.get_pixmap(dpi=200)
     img_bytes = pix.tobytes("png")
     return Image.open(BytesIO(img_bytes))
 
@@ -544,7 +546,9 @@ async def process_pdf():
         return jsonify({"error": f"Failed to open PDF: {str(e)}"}), 500
 
     # Define the batch size (number of PDF pages processed together in one batch)
-    batch_size = 2
+    # YUBI: debugging, I decrease the batch size to 1 from 2
+    # COME BACK TO THIS AND CHANGE IT LATER
+    batch_size = 1
     num_pages = len(doc)  
 
     # Set maximum number of concurrent batches allowed to avoid overloading downstream resources
